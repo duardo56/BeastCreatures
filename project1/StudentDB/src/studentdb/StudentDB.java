0a7e146155d4cabd243145e9b21ff491426ffb80
Eddie <aguilare36@gator.uhd.edu>
2017-02-22 21:18:51 -0600

    made the foundation for the project we need to work on it now

---
+++ project1/StudentDB/src/studentdb/StudentDB.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package studentdb;
+
+/**
+ *
+ * @author heavy_linux_guy
+ */
+public class StudentDB {
+
+    /**
+     * @param args the command line arguments
+     */
+  
+    
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //create the object for the main menu here 
+        MainMenu menu = new MainMenu(); // object for main menu
+        
+        menu.setVisible(true); //this is for the gui to display on the screen
+       
+        
+       
+    }
+    
+}
